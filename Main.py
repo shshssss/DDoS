01d170b122e4b045f1a5fce465b5b302e77dc665
shshssss <162746169+shshssss@users.noreply.github.com>
2024-03-15 15:05:04 -0400

    Main.py

--- Main.py
+++ Main.py
@@ -5,4 +5,8 @@ print("Ok sir good choice the port i will be useing is port 80 on " + ipaddress 
 Duration = input("And how long will you like this to last sir >")
 
 while True:
-    print("Packet sent to" + ipaddress + " With port 80")
+    print("Packet sent to " + ipaddress + " With port 80")
+
+
+#Hello devs this is indeed a fake this my first python project you aim to make a real ddos service in the future if you have any tips 
+#Please contact my discord ox_oop thank you
