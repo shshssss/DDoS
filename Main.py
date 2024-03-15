cb43914216778c98a560cf9a9c09eb86b866df1c
shshssss <162746169+shshssss@users.noreply.github.com>
2024-03-15 14:53:46 -0400

    Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+ipaddress = input("Hello sir, whould you like to DDoS any ips tp day. >")
+
+print("Ok sir good choice the port i will be useing is port 80 on " + ipaddress + "Hope you are ok with that")
+
+Duration = input("And how long will you like this to last sir >")
+
+while True:
+    print("Packet sent to" + ipaddress + " With port 80")
